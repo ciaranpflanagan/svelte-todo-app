9ecae53067aa4c5a18fb6d19448e3665632ac131
Ciaran Flanagan <ciaran@cicisoft.net>
2021-04-23 21:04:14 +0100

    Started Item component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script lang="ts">
-	export let name: string;
+	import Item from './Item.svelte';
+
+	interface TodoItem {
+		id: number,
+		title: string,
+		description: string
+	}
+
+	let item: TodoItem = {
+		id: 1,
+		title: 'Item 1 from data',
+		description: 'Item 1 description from data'
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<section class="section">
+	<div class="container">
+		<div class="columns">
+			<div class="column is-half is-offset-one-quarter main-column">
+				<Item title="Item 1" />
+				<Item title="Item 2" />
+				<Item title="Item 3" />
+			</div>
+		</div>
+	</div>
+</section>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.main-column {
+	text-align: center;
+}
 </style>
---
+++ src/Item.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let title: string;
+    export let description: string = 'No description given';
+</script>
+
+<div class="card mb-4">
+  <div class="card-content">
+    <div class="content mb-4">
+        <h1>{ title }</h1>
+        <p>
+            { description }
+        </p>
+    </div>
+    <div class="columns">
+        <div class="column is-one-quarter is-offset-one-quarter">
+            <button class="button is-success">
+                Complete
+            </button>
+        </div>
+        <div class="column is-one-quarter">
+            <button class="button is-danger">
+                Remove
+            </button>
+        </div>
+    </div>
+  </div>
+</div>
+
+<style>
+
+</style>
